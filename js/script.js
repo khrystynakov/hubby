5f2226a2ae9d044d2ac2fa293c4015a2a154af77
khrystynakov <159631651+khrystynakov@users.noreply.github.com>
2024-02-11 22:26:14 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,20 +3,25 @@ document.getElementById("yes").addEventListener("click", function () {
     document.getElementById("caption").innerHTML = "Hasta la vista Baby!!!!!";
 });
 let previousImageScr = "images/main_bunny.gif";
-function changeImage(isHovered) {
+function changeImage(isMouseOver) {
     const dynamicImage = document.getElementById("dynamicImage");
-    if (isHovered) {
+    if (isMouseOver) {
         previousImageScr = dynamicImage.src;
         dynamicImage.src = "images/happy_bunny.gif";
+        timeoutId = setTimeout(() => 5000);
     } else {
         dynamicImage.src = previousImageScr;
     }
 };
-var noButton = document.getElementById("no");
-function changeImage(isMouseOver) {
-    if (isMouseOver) {
-        var randomLeft = Math.floor(Math.random() * 200);
-        var randomTop = Math.floor(Math.random() * 150);
+var noButton = document.getElementById("noButton");
+function moveButtonOnHover(isHovered) {
+    if (isHovered) {
+        var randomLeft= Math.floor(Math.random() * 500);
+        var randomTop= Math.floor(Math.random() * 100);
         noButton.style.transform = `translate(${randomLeft}px, ${randomTop}px)`;
     } 
 }
+function handleHover(isHovered) {
+    changeImage(isHovered);
+    moveButtonOnHover(isHovered);
+  }
