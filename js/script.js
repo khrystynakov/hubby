848f97ddd3ac69651995b096bdff99fd0631cbff
khrystynakov <159631651+khrystynakov@users.noreply.github.com>
2024-02-11 16:28:01 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,26 +1,22 @@
 document.getElementById("yes").addEventListener("click", function () {
-    document.getElementById("dynamicImage"). src="images/sad_bunny.gif";
-    document.getElementById("caption"). innerHTML="Hasta la vista Baby!!!!!";
+    document.getElementById("dynamicImage").src = "images/sad_bunny.gif";
+    document.getElementById("caption").innerHTML = "Hasta la vista Baby!!!!!";
 });
-let previousImageScr="images/main_bunny.gif";
-function changeImage (isHovered){
-    const dynamicImage=document.getElementById("dynamicImage");
-    if (isHovered){
-        previousImageScr=dynamicImage.src;
-        dynamicImage.src="images/happy_bunny.gif";
+let previousImageScr = "images/main_bunny.gif";
+function changeImage(isHovered) {
+    const dynamicImage = document.getElementById("dynamicImage");
+    if (isHovered) {
+        previousImageScr = dynamicImage.src;
+        dynamicImage.src = "images/happy_bunny.gif";
     } else {
-        dynamicImage.src=previousImageScr;
+        dynamicImage.src = previousImageScr;
     }
 };
-/*function moveButton(){
-    const button=document.getElementById("no");
-    const container=dicument.getElementById("buttonContainer");
-    const containerRect=container.getBoudingClientRect();
-    const randomX=Math.floor(Math.random()*(containerRect.width - 50));
-    button.style.transform= 'translate(${randomX}px, - 40px)';
-}*/
-let hoverCount = 0;
-function incrementHoverCount() {
-    hoverCount++;
-    document.getElementById("hoverCount").innerText = hoverCount;
-  }
+var noButton = document.getElementById("no");
+function changeImage(isMouseOver) {
+    if (isMouseOver) {
+        var randomLeft = Math.floor(Math.random() * 200);
+        var randomTop = Math.floor(Math.random() * 150);
+        noButton.style.transform = `translate(${randomLeft}px, ${randomTop}px)`;
+    } 
+}
